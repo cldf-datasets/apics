4270d47f2b9f36121a2a801ebcd075adf4830e0a
Robert Forkel <xrotwang@googlemail.com>
2026-07-12 16:42:59 +0200

    new cldf layout

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 
-def test_valid(cldf_dataset, cldf_logger):
+def test_valid(cldf_dataset, cldf_sqlite_database, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+    assert cldf_sqlite_database.query('select count(*) from MediaTable')[0][0] == 750
